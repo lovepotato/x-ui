23b3388f49406e1c0330fb312cd7066f8bcfdeed
xmc <659362770@qq.com>
2019-02-20 11:20:39 +0800

    更改打包配置

--- config/index.js
+++ config/index.js
@@ -41,7 +41,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: [path.resolve(__dirname, '../dist'), path.resolve(__dirname, '../demo')],
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
 
